9fff1dc615ccdb088ec7be1c50f559421b666bc1
17huiwei <17huiwei@users.noreply.github.com>
2022-11-26 12:52:34 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Public/视频学习 - 十方教育'],
+  protectedRoutes: ['/视频学习 - 十方教育'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:loren313@gmail.com',
